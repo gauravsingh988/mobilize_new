502b0ae3362dcbf4d37ceea168be504e7137fddb
Gaurav Singh <Gaurav Singh>
2024-05-02 15:00:02 +0530

    project setup

---
+++ src/app/minisidebar/minisidebar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-minisidebar',
+  templateUrl: './minisidebar.component.html',
+  styleUrl: './minisidebar.component.css'
+})
+export class MinisidebarComponent {
+
+}
